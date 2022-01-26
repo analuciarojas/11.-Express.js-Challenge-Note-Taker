21add975122c2a7dc118ecc24884d22cf11a2dee
AnaLucia Rojas <analuciarojas@AnaLucias-MacBook-Pro.local>
2022-01-25 19:04:57 -0800

    updated routes

--- routes/api-route.js
+++ routes/api-route.js
@@ -10,4 +10,12 @@ module.exports = function (app) {
     res.json(db);
   });
 
+  app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+
+  // If no matching route is found default to home
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
 };
